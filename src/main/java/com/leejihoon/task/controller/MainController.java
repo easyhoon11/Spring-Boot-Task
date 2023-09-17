9aa8843f50395c7bbc9b8a1c00ea6e0b558ec495
easy <leege5664@gmail.com>
2023-09-17 23:16:02 +0900

    20230917 @NotBlank

--- src/main/java/com/leejihoon/task/controller/MainController.java
+++ src/main/java/com/leejihoon/task/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.leejihoon.task.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,13 +23,13 @@ public class MainController {
     }
 
     @PostMapping("task")
-    public String postSchedule(@RequestBody PostRequestBodyDto bodyDto){
+    public String postSchedule(@Valid @RequestBody PostRequestBodyDto bodyDto){
         return "일정 작성하기" + " " + bodyDto.getScheduleName() + " " + bodyDto.getCategory() + " " + bodyDto.getDescription() + " " + bodyDto.getDate() + " " + bodyDto.getTime();
     }
 
     @PatchMapping("task/{taskNumber}")
-    public String patchSchedule(@RequestBody PostRequestBodyDto bodyDto){
-         return "일정 수정하기" + " " + bodyDto.getScheduleName() + " " + bodyDto.getCategory() + " " + bodyDto.getDescription() + " " + bodyDto.getDate() + " " + bodyDto.getTime();
+    public String patchSchedule(@Valid @RequestBody PostRequestBodyDto bodyDto){
+        return "일정 수정하기" + " " + bodyDto.getScheduleName() + " " + bodyDto.getCategory() + " " + bodyDto.getDescription() + " " + bodyDto.getDate() + " " + bodyDto.getTime();
     }
 
     @DeleteMapping("task/{taskNumber}")
@@ -38,11 +40,3 @@ public class MainController {
 
 
 
-// 5. Task 컨트롤러에 
-// 일정 불러오기 (GET 프로토콜://호스트:포트/api/v1/task/{taskNumber}), 
-// 일정 작성하기 (POST 프로토콜://호스트:포트/api/v1/task), 
-// 일정 수정하기 (PATCH 프로토콜://호스트:포트/api/v1/task/{taskNumber}), 
-// 일정 삭제하기 (DELETE 프로토콜://호스트:포트/api/v1/task/{taskNumber}) 메서드를 작성한다.
-
-// 5 - 1. 일정 작성하기 요청에서는 데이터를 '일정이름', '카테고리', '설명', '날짜', '시간'을 입력받는다. (모두 문자열)
-// 5 - 2. 일정 수정하기 요청에서는 데이터를 '일정이름', '카테고리', '설명', '날짜', '시간'을 입력받는다. (모두 문자열)
--- src/main/java/com/leejihoon/task/dto/request/PostRequestBodyDto.java
+++ src/main/java/com/leejihoon/task/dto/request/PostRequestBodyDto.java
@@ -1,18 +1,25 @@
 package com.leejihoon.task.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import javax.validation.constraints.NotBlank;
 
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostRequestBodyDto {
+    
+    @NotBlank
     private String scheduleName;
+    @NotBlank
     private String category;
+    @NotBlank
     private String description;
+    @NotBlank
     private String date;
+    @NotBlank
     private String time;
 }
